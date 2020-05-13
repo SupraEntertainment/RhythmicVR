935658d722bef7095cad0547e0d7e0419959ffc7
Tayou <tayou@gmx.net>
2020-05-13 19:51:47 +0200

    added proper cube moving (despawning still missing), minor tweaks

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,19 +12,20 @@ public class GameManager : MonoBehaviour {
     public Material rightMaterial;
     public Material centerMaterial;
     public float spawnDistance;
+    public float speed = 0.1f;
 
     // Start is called before the first frame update
     void Start() {
-        
     }
 
     // Update is called once per frame
-    void Update() {
-        SpawnCube(0.1f, Random.value *2 - 1, Random.value *2 - 1, Random.value *2 - 1, Random.value *2 - 1, (int)Math.Floor(Random.value *3));
+    void FixedUpdate() {
+        SpawnCube(speed, Random.value *2 - 1, /*Random.value *2 - 1*/ 0, Random.value *360, Random.value *360, (int)Math.Floor(Random.value *3));
     }
 
     public void SpawnCube(float speed, float xCoord, float yCoord, float viewRotation, float playspaceRoation, int hand) {
         GameObject cube = Instantiate(cutCube, new Vector3(xCoord, yCoord, spawnDistance), new Quaternion(0, 0, viewRotation, 0));
+        cube.transform.RotateAround(transform.position, Vector3.up, playspaceRoation);
         switch (hand) {
             case 0:
                 cube.GetComponent<MeshRenderer>().material = leftMaterial;
@@ -36,7 +37,6 @@ public class GameManager : MonoBehaviour {
                 cube.GetComponent<MeshRenderer>().material = centerMaterial;
                 break;
         }
-        cube.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -speed / 1000));
     }
 
     public void SpawnWall(float speed, float xCoord, float yCoord, float viewRotation, float playspaceRoation, float width, float height) {
