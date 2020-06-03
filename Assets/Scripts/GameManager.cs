1012f96a003f3d33c02eaefcc8272607aabb42e9
Tayou <tayou@gmx.net>
2020-06-03 17:46:26 +0200

    made note spawning code functional (currently doesn't respect gamemodes)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -7,7 +8,10 @@ using Random = UnityEngine.Random;
 public class GameManager : MonoBehaviour {
 
     public GameObject target;
+    public static GameObject TARGET;
     public GameObject obstacle;
+    public static GameObject OBSTACLE;
+    
     public static Material AMBIGUOUS_MAT;
     public static Material LEFT_MAT;
     public static Material RIGHT_MAT;
@@ -17,10 +21,16 @@ public class GameManager : MonoBehaviour {
     public Material rightMaterial;
     public Material centerMaterial;
     public float spawnDistance;
+    public static float SPAWN_DISTANCE;
     private Config _config;
 
     // Start is called before the first frame update
     void Start() {
+        TARGET = target;
+        OBSTACLE = obstacle;
+
+        SPAWN_DISTANCE = spawnDistance;
+        
         AMBIGUOUS_MAT = ambiguousMaterial;
         LEFT_MAT = leftMaterial;
         RIGHT_MAT = rightMaterial;
@@ -31,13 +41,29 @@ public class GameManager : MonoBehaviour {
         catch (Exception e) {
             Console.WriteLine(e);
             _config = new Config();
-            throw;
         }
+        
+        var bm = new Beatmap();
+        List<Note> notes = new List<Note>();
+        for (int i = 0; i < 240; i++) {
+            var note = new Note();
+            note.time = Random.Range(i/4f-0.5f,i/4f+0.5f);
+            note.rotation = i * 6;
+            note.type = (int) Math.Floor(Random.value * 4);
+            note.xPos = Random.value * 2 - 1;
+            note.yPos = Random.value * 2;
+            notes.Add(note);
+        }
+
+        Debug.Log("generated notes");
+
+        bm.notes = notes.ToArray();
+        StartCoroutine(PlayBeatmap(bm));
     }
 
     // Update is called once per frame
     private void FixedUpdate() {
-        SpawnTarget(1f, Random.value *2 - 1, Random.value *2, Random.value *360, /*Random.value *360*/ 0, (int)Math.Floor(Random.value *4));
+        //SpawnTarget(1f, Random.value *2 - 1, Random.value *2, Random.value *360, /*Random.value *360*/ 0, );
     }
 
     public void SetTrackedObjects(TrackedDevicePair[] tdps) {
@@ -73,12 +99,15 @@ public class GameManager : MonoBehaviour {
     }
 
     private static IEnumerator PlayBeatmap(Beatmap bm) {
-        var time = Time.time;
-        var beatmapLength = bm.notes[bm.notes.Length].time;
-        while (time <= beatmapLength) {
-            time += Time.deltaTime;
-            
-            yield return null;
+        //var beatmapLength = bm.notes[bm.notes.Length-1].time;
+        float currentTime = 0;
+        Debug.Log("in front of loop");
+        for (int i = 0; i < bm.notes.Length; i++) {
+            Debug.Log("in loop " + i);
+            var note = bm.notes[i];
+            yield return new WaitForSeconds(note.time - currentTime);
+            currentTime = note.time;
+            SpawnTarget(note.speed, note.xPos, note.yPos, note.cutDirection, note.rotation, note.type);
         }
     }
 
@@ -91,12 +120,12 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    public void SpawnTarget(float speed, float xCoord, float yCoord, float viewRotation, float playspaceRoation, int hand) {
-        GameObject cube = Instantiate(target, new Vector3(xCoord, yCoord, spawnDistance), new Quaternion(0, 0, viewRotation, 0));
+    public static void SpawnTarget(float speed, float xCoord, float yCoord, float viewRotation, float playspaceRoation, int hand) {
+        GameObject cube = Instantiate(TARGET, new Vector3(xCoord, yCoord, SPAWN_DISTANCE), new Quaternion(0, 0, viewRotation, 0));
         cube.GetComponent<TargetObject>().InitNote(new Note(1, xCoord, yCoord, speed, hand, viewRotation, playspaceRoation));
     }
 
-    public void SpawnObstacle(float speed, float xCoord, float yCoord, float viewRotation, float playspaceRoation, float width, float height) {
+    public static void SpawnObstacle(float speed, float xCoord, float yCoord, float viewRotation, float playspaceRoation, float width, float height) {
         
     }
 }
