b26082984acf72c0c728036a74b85008ac29429b
Tayou <tayou@gmx.net>
2020-08-06 12:00:38 +0200

    added build script

---
+++ Assets/Scripts/BuildProject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BuildProject : ScriptableObject {
+	public bool isDebug;
+	public string windowsOutDir = "out_win";
+	public string linuxOutDir = "out_lin";
+	
+	public BuildProject() {
+		BuildOptions buildOptions = BuildOptions.None;
+
+		if (isDebug) {
+			windowsOutDir += "_debug";
+			linuxOutDir += "_debug";
+			
+			//set build options
+			buildOptions &= BuildOptions.ConnectWithProfiler & BuildOptions.Development;
+		}
+		
+		//windows
+		BuildPipeline.BuildPlayer(new []{SceneManager.GetSceneByName("inGame").path}, windowsOutDir, BuildTarget.StandaloneWindows64, buildOptions);
+		
+		//linux
+		BuildPipeline.BuildPlayer(new []{SceneManager.GetSceneByName("inGame").path}, linuxOutDir, BuildTarget.StandaloneLinux64, buildOptions);
+	}
+}
