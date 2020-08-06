619b6b68e8d7ff9a18bc62044bfb42854b384c74
Tayou <tayou@gmx.net>
2020-08-06 15:16:12 +0200

    renamed Game scene, added editor window to Build script

--- Assets/Scripts/BuildProject.cs
+++ Assets/Scripts/BuildProject.cs
@@ -1,15 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
+﻿using UnityEditor;
+using UnityEditor.Build.Reporting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class BuildProject : ScriptableObject {
-	public bool isDebug;
+public class BuildProject : EditorWindow {
+	public bool isDebug = false;
 	public string windowsOutDir = "out_win";
 	public string linuxOutDir = "out_lin";
 	
-	public BuildProject() {
+	public static void Build(bool isDebug, string windowsOutDir, string linuxOutDir) {
 		BuildOptions buildOptions = BuildOptions.None;
 
 		if (isDebug) {
@@ -21,9 +20,46 @@ public class BuildProject : ScriptableObject {
 		}
 		
 		//windows
-		BuildPipeline.BuildPlayer(new []{SceneManager.GetSceneByName("inGame").path}, windowsOutDir, BuildTarget.StandaloneWindows64, buildOptions);
+		BuildReport report = BuildPipeline.BuildPlayer(new []{SceneManager.GetSceneByName("Game").path}, windowsOutDir, BuildTarget.StandaloneWindows64, buildOptions);
+		BuildSummary summary = report.summary;
+
+		if (summary.result == BuildResult.Succeeded)
+		{
+			Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+		}
+
+		if (summary.result == BuildResult.Failed)
+		{
+			Debug.Log("Build failed");
+		}
 		
 		//linux
-		BuildPipeline.BuildPlayer(new []{SceneManager.GetSceneByName("inGame").path}, linuxOutDir, BuildTarget.StandaloneLinux64, buildOptions);
+		report = BuildPipeline.BuildPlayer(new []{SceneManager.GetSceneByName("Game").path}, linuxOutDir, BuildTarget.StandaloneLinux64, buildOptions);
+		summary = report.summary;
+
+		if (summary.result == BuildResult.Succeeded)
+		{
+			Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+		}
+
+		if (summary.result == BuildResult.Failed)
+		{
+			Debug.Log("Build failed");
+		}
+	}
+
+	[MenuItem("Build/Build Project")]
+	public static void ShowWindow() {
+		GetWindow(typeof(BuildProject));
+	}
+
+	private void OnGUI() {
+		isDebug = EditorGUILayout.Toggle("Is Debug Build", isDebug);
+		windowsOutDir = EditorGUILayout.TextField("Windows Build Directory", windowsOutDir);
+		linuxOutDir = EditorGUILayout.TextField("Linux Build Directory", linuxOutDir);
+
+		if (GUILayout.Button("Build")) {
+			Build(isDebug, windowsOutDir, linuxOutDir);
+		}
 	}
 }
